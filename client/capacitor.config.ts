a3d38962b4d0fdac592eb0c43b4b20203649645f
Jaden Tam <jadenseantam@gmail.com>
2026-07-29 11:21:55 +0800

    Added capacitor

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jdt.habittracker',
+  appName: 'Habit Tracker',
+  webDir: 'www'
+};
+
+export default config;
